865bd030cdb81f9d38a7a049739587397f7359bf
Varun Kumar <heysadboy@gmail.com>
2026-05-01 21:12:11 +0200

    Update initial files
    - Add type for pokemon
    - Add main.tsx

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Gotta catch em' all</div>;
+};
+
+export default App;
---
+++ src/types/pokemon.ts
@@ -0,0 +1,6 @@
+export interface Pokemon {
+  name: string
+  image: string
+  types: string[]
+  moves: string[]
+}
