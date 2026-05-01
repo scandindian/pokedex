3317cfc1cea6853728fc635351558de9d353468f
Varun Kumar <heysadboy@gmail.com>
2026-05-02 01:02:39 +0200

    Update image size

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -9,7 +9,7 @@ const PokemonCard = ({ pokemon }: Props) => {
     <div className="border rounded p-4 shadow-md text-center">
       <h2 className="text-xl font-bold capitalize">{pokemon.name}</h2>
 
-      <img src={pokemon.image} alt={pokemon.name} className="mx-auto" />
+      <img src={pokemon.image} alt={pokemon.name} className="mx-auto w-40 h-40" />
 
       <div>
         <strong>Types:</strong> {pokemon.types.join(", ")}
